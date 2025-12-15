fccd54aa78ce7ea800e449d87f8c167be442fe68
madoka <uki.civilization@gmail.com>
2025-12-16 08:50:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-import streamlit as st
 from ui.components import header, input_area
 
-st.set_page_config(page_title="My App")
-
 header()
-
-email, password, extras = input_area()
-
-st.write("email:", email)
-st.write("password:", password)
-st.write("extras:", extras)
+email, password = input_area()
