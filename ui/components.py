2cd2542e14d161adec9841c2c2fcb573e277786b
madoka <uki.civilization@gmail.com>
2025-12-16 08:51:49 +0900

    Update components.py

--- ui/components.py
+++ ui/components.py
@@ -1,10 +1,22 @@
-# ui/components.py
 import streamlit as st
 
 def header():
-    st.title("ヘッダー")
+    st.title("ログイン画面")
+    st.caption("Streamlit UI サンプル")
 
 def input_area():
-    email = st.text_input("メール")
+    email = st.text_input("メールアドレス")
     password = st.text_input("パスワード", type="password")
-    return email, password
+
+    show_extra = st.checkbox("追加情報を入力する")
+
+    extras = []
+
+    if show_extra:
+        st.subheader("追加情報")
+        extras.append(st.text_input("追加①", key="extra1"))
+        extras.append(st.text_input("追加②", key="extra2"))
+        extras.append(st.text_input("追加③", key="extra3"))
+        extras.append(st.text_input("追加④", key="extra4"))
+
+    return email, password, extras
