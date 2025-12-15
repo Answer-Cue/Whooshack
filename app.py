c325adb9ceb99d136e81e43ab4bf2a72671ca313
madoka <uki.civilization@gmail.com>
2025-12-16 08:52:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+import streamlit as st
 from ui.components import header, input_area
 
+st.set_page_config(page_title="Whooshack", layout="centered")
+
 header()
-email, password = input_area()
+
+email, password, extras = input_area()
+
+if st.button("送信"):
+    st.write("メールアドレス:", email)
+    st.write("パスワード:", "●" * len(password))
+
+    if extras:
+        st.write("追加情報:")
+        for i, ex in enumerate(extras, 1):
+            st.write(f"{i}:", ex)
