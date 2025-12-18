50372c593e40df26e1af7034a4561c5bd88c005b
madoka <uki.civilization@gmail.com>
2025-12-18 18:18:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
 import streamlit as st
-from ui.components import header, input_area
 from map_component import map_component
 
-st.set_page_config(page_title="Whooshack", layout="centered")
+st.title("Map Test")
 
-header()
-
-email, password, extras, checkbox = input_area()
-
-if st.button("送信"):
-    st.write("メールアドレス:", email)
-    st.write("パスワード:", "●" * len(password))
-
-st.subheader("地図")
 location = map_component()
 
-if location is not None:
-    st.write("緯度:", location["lat"])
-    st.write("経度:", location["lng"])
+if location:
+    st.write(location)
