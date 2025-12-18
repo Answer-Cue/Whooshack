2112eec3e19d46fca3efcb98aa29c73880f92ae7
madoka <uki.civilization@gmail.com>
2025-12-18 18:43:30 +0900

    Update trader.py

--- trader.py
+++ trader.py
@@ -1,5 +1,10 @@
-import WhooPy.client as Whoo
+def receive_form_data(data: dict):
+    """
+    data に全部入ってくる想定
+    """
+    print("受け取ったデータ:")
+    for k, v in data.items():
+        print(f"{k}: {v}")
 
-class runling:
-  def test(word):
-    print(word)
+    # ここでDB保存・API送信など
+    return True
