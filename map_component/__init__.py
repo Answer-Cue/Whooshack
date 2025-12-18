6a8af4e7912ce00e6706ad7c7cf5e282957cb4a3
madoka <uki.civilization@gmail.com>
2025-12-18 18:13:11 +0900

    Update __init__.py

--- map_component/__init__.py
+++ map_component/__init__.py
@@ -1,10 +1,1 @@
-import streamlit.components.v1 as components
-import pathlib
-
-frontend = pathlib.Path(__file__).parent / "frontend.html"
-
-map_component = components.declare_component(
-    "map_component",
-    path=str(frontend.parent),
-)
-
+from .map_component import map_component
