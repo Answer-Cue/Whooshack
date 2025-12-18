9ea95e2a7c4b1885b46b912858ac1c6f8380ca13
madoka <uki.civilization@gmail.com>
2025-12-18 19:58:45 +0900

    Update components.py

--- ui/components.py
+++ ui/components.py
@@ -10,12 +10,13 @@ def input_area():
 
     show_extra = st.checkbox("追加情報を入力する")
 
-    extras = []
-
+    extras = {}
     if show_extra:
         st.subheader("追加情報")
-        extras.append(st.text_input("滞在時間", key="stayed_at"))
-        extras.append(st.text_input("バッテリー残量", key="battery_level"))
-        extras.append(st.text_input("移動スピード", key="speed"))
+        extras["latitude"] = st.text_input("緯度", key="extra_lat")
+        extras["longitude"] = st.text_input("経度", key="extra_lon")
+        extras["stayed_at"] = st.text_input("滞在時間", key="extra_stayed")
+        extras["battery_level"] = st.text_input("バッテリー残量", key="extra_battery")
+        extras["speed"] = st.text_input("移動スピード", key="extra_speed")
 
     return email, password, extras, show_extra
